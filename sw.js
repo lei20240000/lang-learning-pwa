6bdb5bb964acb5e24802657d4a41dad8725e77ec
lei20240000 <lei20240000@gmail.com>
2026-04-04 16:50:01 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
+const CACHE_NAME = "lang-pwa-v2";
+const CACHE_FILES = [
+  "/pages/login.html",
+  "/pages/index.html",
+  "/pages/follow.html",
+  "/pages/words.html",
+  "/pages/history.html",
+  "/pages/member.html",
+  "/i18n.js"
+];
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CACHE_FILES))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
